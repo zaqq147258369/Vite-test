fa0a779162a020cfc73b5302b5daa9099a94144b
xuy <3099817419@qq.com>
2023-04-28 16:30:54 +0800

    watch,p154

--- vite-project/src/component/Demo2.vue
+++ vite-project/src/component/Demo2.vue
@@ -24,7 +24,37 @@ person.fullName = computed({
 
 watch(sum,(newValue,oldValue)=>{
     console.log('sum变了',newValue,oldValue)
-},{immedite:true})
+},{immedite:true});
+
+/*监视reactive定义的一个响应式数据的全部属性，
+    注意：
+        1、此处无法获取到oldValue
+        2、强制开启了深度监视（deep配置无效）
+*/
+ watch(person,(newValue,oldValue)=>{
+    console.log('person变了',newValue,oldValue)
+},{deep:false});       
+
+
+/*监视reactive定义的一个响应式数据中的某个属性，
+ watch(()=>person.firstName,(newValue,oldValue)=>{
+    console.log('person中的name变了',newValue,oldValue)
+});       
+*/
+
+/*监视reactive定义的一个响应式数据中的某些属性，
+ watch([()=>person.firstName,()=>person.lastName],(newValue,oldValue)=>{
+    console.log('person中的name变了',newValue,oldValue)
+});       
+*/
+
+/*
+特殊情况：
+watch([()=>person.job,()=>person.lastName],(newValue,oldValue)=>{
+    console.log('person中的name变了',newValue,oldValue)
+},{deep:true});       //此处由于监视的是reactive定义的对象中的某个属性，所以deep有效
+
+*/
 
 </script>
 <template>
