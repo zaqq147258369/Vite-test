6aa6ea89d3d2327308f37cb11d1196d71e6ab815
xuyc <3099817419@qq.com>
2023-05-13 23:54:57 +0800

    shallow，p160

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,6 +2,7 @@
 import Demo from './component/Demo.vue'
 import Demo2 from './component/Demo2.vue'
 import Demo3 from './component/Demo3.vue';
+import Demo4 from './component/Demo4.vue';
 import{ref} from 'vue'
 let isShowDemo3 = ref(true)
 </script>
@@ -11,6 +12,7 @@ let isShowDemo3 = ref(true)
     <Demo v-if="false"></Demo>
     <Demo2 v-if="false"></Demo2>
     <button @click="isShowDemo3 = !isShowDemo3">切换隐藏/显示</button> 
-    <Demo3 v-if="isShowDemo3"></Demo3>
+    <Demo3 v-if="false"></Demo3>
+    <Demo4 v-if="isShowDemo3"></Demo4>
   </div>
 </template>
---
+++ vite-project/src/component/Demo4.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {reactive,toRefs,shallowReactive,shallowRef,shallowReadonly,readonly} from 'vue'
+let person = reactive({
+    name:'张三',
+    age:18,
+    job:{
+        j1:{
+            salary:27
+        }
+    }
+})
+let person2 = shallowReactive({})
+let x = shallowRef(0);
+
+// person = readonly(person) //只读数据，深层次限制为只读
+
+person = shallowReadonly(person) //只限制浅层次的数据（person的第一级）。job对象就不会限制为只读。
+
+
+</script>
+
+<template>
+  <div>
+    <h2>姓名：{{person.name}}</h2>
+    <h2>姓名：{{person.age}}</h2>
+    <h2>姓名：{{person.job.j1.salary}}</h2>
+    <button @click="person.name+='~'">修改姓名</button>
+    <button @click="person.age++">增长年龄</button>
+    <button @click="person.job.j1.salary++">涨薪</button>
+  </div>
+
+</template>
+
