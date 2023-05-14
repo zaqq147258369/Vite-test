23048771e59433a3a85be823664637a9f714d34b
xuyc <3099817419@qq.com>
2023-05-14 23:08:04 +0800

    toRaw，p161

--- vite-project/src/component/Demo4.vue
+++ vite-project/src/component/Demo4.vue
@@ -1,5 +1,14 @@
 <script setup>
-import {reactive,toRefs,shallowReactive,shallowRef,shallowReadonly,readonly} from 'vue'
+import {
+  reactive,
+  toRefs,
+  shallowReactive,
+  shallowRef,
+  shallowReadonly,
+  readonly,
+  toRaw,
+  markRaw
+} from 'vue'
 let person = reactive({
     name:'张三',
     age:18,
@@ -13,9 +22,16 @@ let person2 = shallowReactive({})
 let x = shallowRef(0);
 
 // person = readonly(person) //只读数据，深层次限制为只读
+// person = shallowReadonly(person) //只限制浅层次的数据（person的第一级）。job对象就不会限制为只读。
 
-person = shallowReadonly(person) //只限制浅层次的数据（person的第一级）。job对象就不会限制为只读。
+console.log(person);
+const p = toRaw(person) //将reactive定义的响应式对象还原为普通对象
+console.log(p);
 
+function addCar(){
+  let car = {name:'xx',price:40}
+  person.car = markRaw(car) //将car属性标记为一个对象，使其永远不会成为响应式对象。但是可以修改car内容，car对象内部属性修改不会渲染页面。
+}
 
 </script>
 
