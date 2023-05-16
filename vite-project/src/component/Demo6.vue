a63d6dd1863b7fbcc4d94827d9303a2d37c2bec4
xuyc <3099817419@qq.com>
2023-05-16 22:52:34 +0800

    isRef,p166

--- vite-project/src/component/Demo6.vue
+++ vite-project/src/component/Demo6.vue
@@ -1,10 +1,18 @@
 <script setup>
 import Child from './Demo6/Child.vue';
-import { reactive,provide } from 'vue';
+import { reactive,provide, ref,readonly,isRef,isReactive,isReadonly,isProxy } from 'vue';
 let car = reactive({
     name:'小奔',
     price:'40'
 })
+let sum = ref(0)
+let car2 = readonly(car)
+
+console.log(isRef(sum));
+console.log(isReactive(car));
+console.log(isReadonly(car2));
+console.log(isProxy(car));
+console.log(isProxy(sum));
 
 provide('car',car) //给自己的后代组件提供数据
 </script>
