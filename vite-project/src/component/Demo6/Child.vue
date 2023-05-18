4f04f280d9f8f6111cfe3802848f5f2558c9c764
xuyc <3099817419@qq.com>
2023-05-18 22:59:07 +0800

    vue3

--- vite-project/src/component/Demo6/Child.vue
+++ vite-project/src/component/Demo6/Child.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { ref } from 'vue';
 import Son from './Son.vue';
+let title = ref('xxx')
 </script>
 <template>
   <div class="child">
     <h2>Child组件（子组件）</h2>
-    <Son></Son>
+    <h3>接受子组件传过来的值{{}}</h3>
+    <Son :title="title"></Son>
+    
   </div>
 </template>
 <style scoped>
--- vite-project/src/component/Demo6/Son.vue
+++ vite-project/src/component/Demo6/Son.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { inject } from 'vue';
+const propst = defineProps({
+  title:''
+})
+
+console.log(propst.title);
 let car = inject('car')
 </script>
 <template>
   <div class="son">
     <h2>Son组件（孙组件）,{{ car.name }} -- {{ car.price }}</h2>
+    <h2>{{ title }}</h2>
+    <teleport to="body">
+      <div>11</div>
+    </teleport>
   </div>
 </template>
 <style scoped>
